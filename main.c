79c786f813f41180b18e89f0a4cb2aa9acf96219
Henrique Nicolini <h.nicolini1@gmail.com>
2024-09-05 22:31:09 -0300

    Atualização
    Os Usuários Podem ser Cadastrados, com a função de conferir se o mesmo existe ou não existe!

--- main.c
+++ main.c
@@ -1,32 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+<<<<<<< HEAD
+#include <stdbool.h>
+typedef struct{
+    char usuario[80];
+    char senha[80];
+}User;
 int usuario(void);
+int cadastro(void);
+=======
 
+typedef struct bolsa{
+    // declarando as moedas
+    float bitcoin, ethereum, ripple;
+} bolsa;
+
+>>>>>>> 258a9e8753903f5813a0d2f27734ff2507018ad6
 int main(void){
-    usuario();
-    return 0;
+    bolsa moedas;
+    moedas.bitcoin = 312444.10;
+    moedas.ethereum = 16233.20;
+    moedas.ripple = 231.03;
+    // Salvando cotas em arquivo binario
+    FILE* cotas = fopen("cotas", "wb");
+    fwrite(&moedas,sizeof(bolsa),1, cotas);
+    fclose(cotas);
+    // limpando a variavel bitcoin em moedas para teste
+    moedas.bitcoin = 0;
+    moedas.ethereum = 0;
+    printf("Moeda zerada: %.2f\n", moedas.bitcoin);
+    // Lendo as cotas do arquivo binario
+    puts("Conteudo do arquivo");
+    cotas = fopen("cotas", "rb");
+    fread(&moedas, sizeof(bolsa), 1, cotas);
+    printf("bitcoin: %.2f\nethereum: %.2f\nripple: %.2f", moedas.bitcoin, moedas.ethereum, moedas.ripple);
+    fclose(cotas);
+
 }
-int login(void){
+<<<<<<< HEAD
+
+
+int UsuarioExiste(char *usuario_existente){
+    FILE *arquivo_ler = fopen("Usuario", "rb");
+    if(arquivo_ler == NULL){
+        printf("Erro ao abrir o arquivo!\n");
+        return 0;
+    }
+    User user;
+    while(fread(&user, sizeof(User), 1, arquivo_ler)){
+        if(strcmp(user.usuario, usuario_existente) == 0){
+            return 1;
+        } 
+    fclose(arquivo_ler);
+    return 0;         
+    }
 
 }
 int cadastro(void){
     char usuario[80];
     char senha[30];
-    FILE *arquivo = fopen("Usuario", "ab");
-    printf("Digite seu nome de usuario:");
-    fgets(usuario, sizeof(usuario), stdin);
-    fprintf(arquivo,"Usuario: %s",&usuario);
     
     
-    printf("Digite sua senha: \n");
-    fgets(senha, sizeof(senha), stdin), "\n";
-    fprintf(arquivo,"Senha: %s",&senha);
+    printf("Digite o nome de usuario: \n");
+    scanf("%s", &usuario);
+    printf("Digite a Senha: \n");
+    scanf("%s", &senha);
+    if (UsuarioExiste(usuario))
+    {
+        printf("Usuario Existente!\n");
+        cadastro();
+            
+    }else{
+       FILE *arquivo = fopen("Usuario", "ab");
+       User user;
+        strncpy(user.&usuario, usuario, sizeof(user.usuario));
+        strncpy(user.&senha, senha, sizeof(user.senha));
+        fwrite(&user, sizeof(User), 1, arquivo);
+        fclose(arquivo);
+        printf("Usuario Cadastrado com Sucesso\n");
+    }
+}       
+    
+    
 
-     
-
-
-}
 int usuario(void){
     int resposta;
     printf("Ja possui um Login?\n1- Sim\n2- Nao\n");
@@ -45,3 +102,5 @@ int usuario(void){
     }
         
 }
+=======
+>>>>>>> 258a9e8753903f5813a0d2f27734ff2507018ad6
