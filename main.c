223d0290b93c4b9f543b80c5eac6128cda8ed001
KaioLC <kaiolopessilva@gmail.com>
2024-09-05 21:27:21 -0300

    Parte de salvar e ler as moedas
    O arquivo cotas é um arquivo binario, é usado para salvar e ler os valores das moedas.

--- main.c
+++ main.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
+typedef struct bolsa{
+    // declarando as moedas
+    float bitcoin, ethereum, ripple;
+} bolsa;
+
 int main(void){
-    aloalao
+    bolsa moedas;
+    moedas.bitcoin = 312444.10;
+    moedas.ethereum = 16233.20;
+    moedas.ripple = 231.03;
+    // Salvando cotas em arquivo binario
+    FILE* cotas = fopen("cotas", "wb");
+    fwrite(&moedas,sizeof(bolsa),1, cotas);
+    fclose(cotas);
+    // limpando a variavel bitcoin em moedas para teste
+    moedas.bitcoin = 0;
+    moedas.ethereum = 0;
+    printf("Moeda zerada: %.2f\n", moedas.bitcoin);
+    // Lendo as cotas do arquivo binario
+    puts("Conteudo do arquivo");
+    cotas = fopen("cotas", "rb");
+    fread(&moedas, sizeof(bolsa), 1, cotas);
+    printf("bitcoin: %.2f\nethereum: %.2f\nripple: %.2f", moedas.bitcoin, moedas.ethereum, moedas.ripple);
+    fclose(cotas);
+
 }
