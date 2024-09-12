4f800ab25946a607bb5db9048e583738af6d00d2
KaioLC <kaiolopessilva@gmail.com>
2024-09-12 19:18:02 -0300

    parte de alteracao das criptos

--- main.c
+++ main.c
@@ -1,30 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-typedef struct bolsa{
-    // declarando as moedas
+typedef struct BolsaCripto{
     float bitcoin, ethereum, ripple;
-} bolsa;
+} BolsaCripto;
+
+void salvar_cota(BolsaCripto* valores){
+    FILE* cotas = fopen("cotas", "wb");
+    // Salvando cotas em arquivo binario
+    if(cotas == NULL){
+        printf("Não foi possivel abrir o arquivo para escrever");
+    }
+    fwrite(valores,sizeof(BolsaCripto),1, cotas);
+    fclose(cotas);
+}
+void ler_cota(BolsaCripto* valores){
+    FILE* cotas = fopen("cotas", "rb");
+    if(cotas == NULL){
+        printf("Não foi possivel abrir o arquivo para ler");
+    }
+    fread(valores, sizeof(BolsaCripto), 1, cotas);
+    fclose(cotas);
+}
+void alterar_valor_moeda(BolsaCripto valores){
+    // gerando o numero aleatorio entre -5% e 5%
+    srand(time(NULL));
+    float numero_gerado =((rand() % 5) + 1.0)/100.0;
+    int escolhe_sinal = rand()% 2;
+    if(escolhe_sinal == 1){
+        numero_gerado*=-1;
+    }
+
+    //  calculando os bytes para percorrer os valores 1 por 1 (4 em 4 bytes) na struct
+   size_t len_bolsa = sizeof(valores);
+   for(size_t indice = 0; indice < len_bolsa; indice += sizeof(float)){
+        float* pos_bolsa = (float*)((char*)&valores + indice); // char (1 byte) de moedas é somado com valor atual de indice, após e realizado conversão para float, podendo caminhar pelas variaveis floats(4 bytes) do struct
+
+        *pos_bolsa += (*pos_bolsa)*numero_gerado;
+   }
+   salvar_cota(&valores);
+    
+}
+
 
 int main(void){
-    bolsa moedas;
-    moedas.bitcoin = 312444.10;
-    moedas.ethereum = 16233.20;
-    moedas.ripple = 231.03;
-    // Salvando cotas em arquivo binario
-    FILE* cotas = fopen("cotas", "wb");
-    fwrite(&moedas,sizeof(bolsa),1, cotas);
-    fclose(cotas);
-    // limpando a variavel bitcoin em moedas para teste
-    moedas.bitcoin = 0;
-    moedas.ethereum = 0;
-    printf("Moeda zerada: %.2f\n", moedas.bitcoin);
+    BolsaCripto moedas;
+    // moedas.bitcoin = 330667.00;
+    // moedas.ethereum = 13443.73;
+    // moedas.ripple = 3.05;
+    //salvar_cota(&moedas);
+
     // Lendo as cotas do arquivo binario
     puts("Conteudo do arquivo");
-    cotas = fopen("cotas", "rb");
-    fread(&moedas, sizeof(bolsa), 1, cotas);
-    printf("bitcoin: %.2f\nethereum: %.2f\nripple: %.2f", moedas.bitcoin, moedas.ethereum, moedas.ripple);
-    fclose(cotas);
+    ler_cota(&moedas);
+    printf("bitcoin: %.2f\nethereum: %.2f\nripple: %.2f\n", moedas.bitcoin, moedas.ethereum, moedas.ripple);
+
+
+    alterar_valor_moeda(moedas);
+    ler_cota(&moedas); // salvando valor alterado
+    puts("\nApos alterar valor");
+    printf("\nbitcoin: %.2f\nethereum: %.2f\nripple: %.2f", moedas.bitcoin, moedas.ethereum, moedas.ripple);
+
+    
     return 0;
 }
